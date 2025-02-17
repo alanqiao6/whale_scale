ce50cbf128558ba469428b3b158f60c7902826e4
Ciaran Burr <ciaranburr2005@gmail.com>
2025-02-17 00:07:06 -0500

    added in measure and xcertainty buttons to toggle between components

--- whale_scale/frontend/src/App.js
+++ whale_scale/frontend/src/App.js
@@ -9,6 +9,7 @@ import Measure from './Measure';
 
 function App() {
     const [message, setMessage] = useState("");
+    const [isMeasureVisible, setIsMeasureVisible] = useState(true); 
 
     useEffect(() => {
     axios.get('/home/')
@@ -18,11 +19,25 @@ function App() {
         .catch(error => console.error(error));
     }, []);
 
+    const toggleToMeasure = () => {
+        setIsMeasureVisible(true);
+    };
+
+    const toggleToXcertainty = () => {
+        setIsMeasureVisible(false); 
+    };
     return (
         <div className="app-container">
             <Navbar /> {/* Learned how to create different components here: https://www.youtube.com/watch?v=0sSYmRImgRY */}
             <Data /> 
-            <Measure />
+            <div className="toggle-buttons">
+                <button onClick={toggleToMeasure}>Show Measure</button>
+                <button onClick={toggleToXcertainty}>Show Xcertainty</button>
+            </div>
+
+            {isMeasureVisible ? 
+                <Measure toggleToXcertainty={toggleToXcertainty} /> : 
+                <Xcertainty toggleToMeasure={toggleToMeasure} />}
             {/*<div className="content">
                 <h1>{message}</h1> 
                 <p>temporary content</p>
--- whale_scale/frontend/src/Xcertainty.js
+++ whale_scale/frontend/src/Xcertainty.js
@@ -1,9 +1,11 @@
 import './Xcertainty.css';
 import React from 'react';
 
-const Xcertainty = () => {
+const Xcertainty = ({toggleToXcertainty, toggleToMeasure}) => {
     return (
         <div className="xcertainty">
+            <button onClick={toggleToMeasure}>Switch to Measure</button>
+            <button onClick={toggleToXcertainty}>Switch to Xcertainty</button>
             <p>Xcertainty</p>
         </div>
     );
