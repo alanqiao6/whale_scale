2bba887394ee60018b8a3d34d3bc83f792962d2f
Alan Qiao <ayq2@duke.edu>
2025-02-16 23:12:35 -0500

    Added filler text for Measure and Xcertainty components

---
+++ whale_scale/frontend/src/Xcertainty.js
@@ -0,0 +1,12 @@
+import './Xcertainty.css';
+import React from 'react';
+
+const Xcertainty = () => {
+    return (
+        <div className="xcertainty">
+            <p>Xcertainty</p>
+        </div>
+    );
+}
+
+export default Xcertainty;
