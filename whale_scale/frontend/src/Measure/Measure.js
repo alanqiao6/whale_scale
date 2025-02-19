a09bc5007ae7440b631089e11110eb69306903a0
Ciaran Burr <ciaranburr2005@gmail.com>
2025-02-19 01:19:11 -0500

    added in temporary images for spring 2 presentation

--- whale_scale/frontend/src/Measure/Measure.js
+++ whale_scale/frontend/src/Measure/Measure.js
@@ -1,5 +1,6 @@
 import './Measure.css';
 import React from 'react';
+import whaleDrawing from '../Images/whale_drawing.png';
 
 const Measure = ({ toggleToXcertainty, toggleToMeasure}) => {
     return (
@@ -7,6 +8,7 @@ const Measure = ({ toggleToXcertainty, toggleToMeasure}) => {
             <button className="toggle" onClick={toggleToMeasure}>Switch to Measure</button>
             <button className="toggle" onClick={toggleToXcertainty}>Switch to Xcertainty</button>
             <p>Measure</p>
+            <img src={whaleDrawing} alt="Whale Drawing" className="whale-image" />
         </div>
     );
 }
--- whale_scale/frontend/src/Xcertainty/Xcertainty.js
+++ whale_scale/frontend/src/Xcertainty/Xcertainty.js
@@ -1,5 +1,7 @@
 import './Xcertainty.css';
 import React from 'react';
+import XcertDrawing from '../Images/xcertainty_placeholder.png';
+
 
 const Xcertainty = ({toggleToXcertainty, toggleToMeasure}) => {
     return (
@@ -7,6 +9,7 @@ const Xcertainty = ({toggleToXcertainty, toggleToMeasure}) => {
             <button className="toggle" onClick={toggleToMeasure}>Switch to Measure</button>
             <button className="toggle" onClick={toggleToXcertainty}>Switch to Xcertainty</button>
             <p>Xcertainty</p>
+            <img src={XcertDrawing} alt="Temporary Xcertainty" className="xcert-image" />
         </div>
     );
 }
