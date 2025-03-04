a90bd7ba033e3cc74c7428984d6720b73952a749
Jason Fitzpatrick <jasonfitz01@yahoo.com>
2025-03-04 13:04:31 -0500

    Added placeholder API endpoints for MorphometriX

--- whale_scale/main/views.py
+++ whale_scale/main/views.py
@@ -1,5 +1,65 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def index(request):
     return JsonResponse({"message": "Hello World!"})
+
+# ----------------------
+# MorphoMetriX Endpoints
+# ----------------------
+class MorphoMetriX(View):
+    '''
+    API Endpoints for photogrammetry measurement tasks using MorphoMetriX.
+    '''
+
+    @csrf_exempt
+    def measure_length(self, request):
+        """
+        Measure length from an image.
+        Input:
+            - image_path: str (Path to the image)
+            - points: list of tuples [(x1, y1), (x2, y2)] (Pixel coordinates for measurement)
+        Output:
+            - length: float (Measured length in pixels)
+        """
+        pass
+
+    @csrf_exempt
+    def measure_width(self, request):
+        """
+        Measure width from an image.
+        Input:
+            - image_path: str
+            - reference_line: list of tuples [(x1, y1), (x2, y2)]
+        Output:
+            - width: float (Measured width in pixels)
+        """
+        pass
+
+    @csrf_exempt
+    def measure_angle(self, request):
+        """
+        Measure an angle between three points.
+        Input:
+            - image_path: str
+            - points: list of tuples [(x1, y1), (x2, y2), (x3, y3)]
+        Output:
+            - angle: float (Measured angle in degrees)
+        """
+        pass
+
+    @csrf_exempt
+    def measure_area(self, request):
+        """
+        Measure area based on polygon selection.
+        Input:
+            - image_path: str
+            - polygon: list of tuples [(x1, y1), (x2, y2), ..., (xn, yn)]
+        Output:
+            - area: float (Measured area in pixels²)
+        """
+        pass
