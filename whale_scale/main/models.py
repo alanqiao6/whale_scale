c2b67bbf97d60bf45a08a2df8eb38d01479bfa93
Ciaran Burr <ciaranburr2005@gmail.com>
2025-03-24 02:16:50 -0400

    added in models to create database

--- whale_scale/main/models.py
+++ whale_scale/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class UserCredential(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=255)  # Store hashed passwords for security
+
+    def __str__(self):
+        return self.username
