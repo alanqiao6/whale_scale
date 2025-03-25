fe19022af074902ae696abbef094792b4f5cdf3d
Ciaran Burr <ciaranburr2005@gmail.com>
2025-03-25 04:12:13 -0400

    added admin view of tables using django

--- whale_scale/main/admin.py
+++ whale_scale/main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from main.models import UserCredential  # Replace 'main' with the correct app name
 
 # Register your models here.
+admin.site.register(UserCredential)
