47a34f26e38bfce2377b49e3e2865ce92b3f8b14
Ciaran Burr <ciaranburr2005@gmail.com>
2025-04-02 02:06:43 -0400

    tried to fix settings for deployment

--- whale_scale/whale_scale/settings.py
+++ whale_scale/whale_scale/settings.py
@@ -11,13 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+
 import os
-
-# Define ENV from environment variable
-ENV = os.environ.get('ENVIRONMENT', 'dev')  # Default to 'dev' if not set
-
-import socket
-socket.setdefaulttimeout(300)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)r*pe-1hya)%_1z14@**%fx0(!7kwxevp=inzzv@g291^5v3(i"
+ENV = os.environ.get('ENVIRONMENT', 'dev')  # 'prod' or 'dev'
 
 DEBUG = os.environ.get(f'{ENV.upper()}_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get(f'{ENV.upper()}_ALLOWED_HOSTS', 'localhost').split(',')
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure1234')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'media')
@@ -81,11 +77,14 @@ WSGI_APPLICATION = "whale_scale.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
