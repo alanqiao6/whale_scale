b27939b8eab123e29cdac2ed7258e85f5dfb447a
Ciaran Burr <ciaranburr2005@gmail.com>
2025-04-02 12:07:30 -0400

    Revert "Edit Data.js to see if pipeline functional"
    This reverts commit 6c6b0353c04902e76187fa8c8cfa897779810d73.

--- whale_scale/frontend/src/components/Data.js
+++ whale_scale/frontend/src/components/Data.js
@@ -4,7 +4,7 @@ import React from "react"
 export default function Data() {
   return (
     <div className="data-section">
-      <h2>Data Test</h2>
+      <h2>Data</h2>
       <div className="data-content">{/* Data content will be populated here */}</div>
     </div>
   )
