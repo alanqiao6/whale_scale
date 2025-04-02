10a94ef471d78fa57d89b6cd0fc8ab7f4c628453
August Hao <ah670@duke.edu>
2025-04-02 00:12:05 -0400

    old settings.py

--- whale_scale/whale_scale/settings.py
+++ whale_scale/whale_scale/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-import os
+import socket
+socket.setdefaulttimeout(300)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-ENV = os.environ.get('ENVIRONMENT', 'dev')  # 'prod' or 'dev'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-)r*pe-1hya)%_1z14@**%fx0(!7kwxevp=inzzv@g291^5v3(i"
 
-DEBUG = os.environ.get(f'{ENV.upper()}_DEBUG', '0') == '1'
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get(f'{ENV.upper()}_ALLOWED_HOSTS', 'localhost').split(',')
-
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure1234')
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -75,14 +76,11 @@ WSGI_APPLICATION = "whale_scale.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
