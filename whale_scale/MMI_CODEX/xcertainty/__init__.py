c66b7b264314fdf0369125a9108989574c485861
Ciaran Burr <ciaranburr2005@gmail.com>
2025-04-30 12:23:09 -0400

    added comments for empty init files"

---
+++ whale_scale/MMI_CODEX/xcertainty/__init__.py
@@ -0,0 +1,10 @@
+"""
+WhaleScale MMI_CODEX Package Initialization
+=======================================
+
+Purpose:
+   Initializes the Marine Mammal Imagery CodeX (MMI_CODEX) package as default Python package
+   
+Author:
+   Alan Qiao, August Hao, Ciaran Burr, Jason Fitzpatrick
+"""
---
+++ whale_scale/whale_scale/__init__.py
@@ -0,0 +1,10 @@
+"""
+WhaleScale Django Package Initialization
+=======================================
+
+Purpose:
+   Marks the directory as a Python package, allowing imports from this package.
+   
+Author:
+   Alan Qiao, August Hao, Ciaran Burr, Jason Fitzpatrick
+"""
